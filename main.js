53db84745a585c68c48b20aea7e7fd25fd925e57
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-18 01:20:30 +0300

    clear code

--- main.js
+++ main.js
@@ -4,28 +4,6 @@ const db=require("./db")
 const {users,articles}=require("./schema")
 const port = 5000;
 app.use(express.json());
-// const { uuid } = require("uuidv4");
-
-// let articles = [
-//   {
-//     id: 1,
-//     title: "How I learn coding?",
-//     description: "Lorem, Quam, mollitia.",
-//     author: "Jouza",
-//   },
-//   {
-//     id: 2,
-//     title: "Coding Best Practices",
-//     description: "Lorem, ipsum dolor sit, Quam, mollitia.",
-//     author: "Besslan",
-//   },
-//   {
-//     id: 3,
-//     title: "Debugging",
-//     description: "Lorem, Quam, mollitia.",
-//     author: "Jouza",
-//   },
-// ];
 
 
 //getAllArticles
