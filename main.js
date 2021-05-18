423f33156540fddd63ae67987cbc3fca0af98ed3
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-05-18 14:05:00 +0300

    clear code

--- main.js
+++ main.js
@@ -1,95 +1,94 @@
 const express = require("express");
 const app = express();
-const db=require("./db")
-const {users,articles,comments}=require("./schema")
+const db = require("./db");
+const { users, articles, comments } = require("./schema");
 const port = 5000;
 app.use(express.json());
-
 
 //getAllArticles
 app.get("/articles", (req, res) => {
   res.status(200);
   articles
-  .find()
-  .then((result)=>{
-    res.json(result)
-  })
-  .catch((err)=>{
-    res.send(err);
-  })
-})
+    .find()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 //getArticlesByAuthor
 app.get("/articles/search_1", (req, res) => {
   let author_id = req.query.author;
-  articles.find({author:author_id}).then((result)=>{
-    res.json(result)
-  })
-  .catch((err)=>{
-    res.send(err)
-  })
+  articles
+    .find({ author: author_id })
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 
   res.status(200);
 });
-  
 
 //getAnArticleById
 app.get("/articles/:id", (req, res) => {
   let _id = req.body.id;
-  articles.find({_id}).populate("author_id","author")
-  .exec()
-  .then((result)=>{
-    res.json(result)
-  })
-  .catch((err)=>{
-    res.send(err)
-  })
+  articles
+    .find({ _id })
+    .populate("author_id", "author")
+    .exec()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
   res.status(200);
 });
 
 //createNewArticle
 app.post("/articles", (req, res) => {
-
-const {title,description,author}= req.body
+  const { title, description, author } = req.body;
 
   const newArticle = new articles({
     title,
     description,
-    author
-});
+    author,
+  });
 
-newArticle
-.save()
+  newArticle
+    .save()
     .then((result1) => {
       res.json(result1);
     })
     .catch((err) => {
       res.json(err);
     });
-})
+});
 
 //updateAnArticleById
 app.put("/articles/id", (req, res) => {
-  const {_id,title, description} = req.body;
-  const found = articles.findOneAndUpdate({_id},
-  { title, description},{new:true}
-  )
+  const { _id, title, description } = req.body;
+  const found = articles
+    .findOneAndUpdate({ _id }, { title, description }, { new: true })
 
-  .then((result) => {
-    res.json(result);
-  })
-  .catch((err) => {
-    res.send(err);
-  });
-  
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 //deleteArticleById
 
 app.delete("/articles/id", (req, res) => {
-  const {_id,title, description} = req.body;
+  const { _id, title, description } = req.body;
   articles
-    .deleteOne({ _id }, {title,description })
+    .deleteOne({ _id }, { title, description })
     .then((result) => {
       res.json(result);
     })
@@ -97,13 +96,12 @@ app.delete("/articles/id", (req, res) => {
       res.send(err);
     });
 });
-
 
 //deleteArticlesByAuthor
 app.delete("/articles", (req, res) => {
-  const {title, description,author} = req.body;
+  const { title, description, author } = req.body;
   articles
-    .deleteOne({ author }, {title,description })
+    .deleteOne({ author }, { title, description })
     .then((result) => {
       res.json(result);
     })
@@ -112,60 +110,58 @@ app.delete("/articles", (req, res) => {
     });
 });
 
-
-
 //2.mongoDB
 //createNewAuthor
-app.post("/users",(req,res) =>{
-let {firstName,lastName,age,country,email,password}=req.body
-const user=new users (
- req.body
-)
-user.save().then((result)=>{
-  res.json(result)
-}).catch((err)=>{
-  res.status(201);
-}) 
-})
-
+app.post("/users", (req, res) => {
+  let { firstName, lastName, age, country, email, password } = req.body;
+  const user = new users(req.body);
+  user
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(201);
+    });
+});
 
 //login level1
-app.post("/login",(req,res)=>{
-  let{email,password}=req.body
-  users.findOne({email,password})
-    .then((result)=>{
-      console.log(result);
-      if(result){
-        res.status(200)
-        res.json("Valid login credentials")
-      }else {
-        res.status(401)
-        res.send("Invalid login credentials") 
+app.post("/login", (req, res) => {
+  let { email, password } = req.body;
+  users.findOne({ email, password }).then((result) => {
+    console.log(result);
+    if (result) {
+      res.status(200);
+      res.json("Valid login credentials");
+    } else {
+      res.status(401);
+      res.send("Invalid login credentials");
     }
-  }) 
-  })
-
+  });
+});
 
 //createNewComment
-app.post("/articles/:id/comments",(req,res)=>{
-let {comment,commenter}=req.body
-const id=req.params.id
-const niceComment =new comments({
-  comment,commenter
-})
+app.post("/articles/:id/comments", (req, res) => {
+  let { comment, commenter } = req.body;
+  const id = req.params.id;
+  const niceComment = new comments({
+    comment,
+    commenter,
+  });
 
-niceComment
-.save().then(async(result)=>{
-await articles.findOneAndUpdate({_id:id},
-{$push:{commentss:result._id}})
-  res.json(result)
-}).catch((err)=>{
-  res.status(201);
-})
-})
-
-
-
+  niceComment
+    .save()
+    .then(async (result) => {
+      await articles.findOneAndUpdate(
+        { _id: id },
+        { $push: { commentss: result._id } }
+      );
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(201);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- schema.js
+++ schema.js
@@ -1,29 +1,28 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  age: { type: Number },
+  country: { type: String },
+  email: { type: String },
+  password: { type: String },
+});
 
-    firstName:{type:String}, 
-    lastName:{type:String},
-    age:{type:Number},
-    country: {type:String},
-    email:{type:String},
-    password: {type:String}
-})
+const articlesSchema = new mongoose.Schema({
+  title: { type: String },
+  description: { type: String },
+  author: { type: mongoose.Schema.ObjectId, ref: "users" },
+  commentss: [{ type: mongoose.Schema.ObjectId, ref: "comment" }],
+});
 
-const articlesSchema= new mongoose.Schema({
-    title:  {type:String},
-    description: {type:String},
-    author:{type:mongoose.Schema.ObjectId,ref:"users"},
-    commentss:[{type:mongoose.Schema.ObjectId,ref:"comment"}]
-})
-
-const commentsSchema =new mongoose.Schema({
-    comment: {type:String},
-    commenter:{type:mongoose.Schema.ObjectId,ref:"users"}
-})
-const users=mongoose.model("users",usersSchema)
-const articles=mongoose.model("articles",articlesSchema)
-const comments =mongoose.model("comment",commentsSchema)
-module.exports.users=users
-module.exports.articles=articles
-module.exports.comments=comments
+const commentsSchema = new mongoose.Schema({
+  comment: { type: String },
+  commenter: { type: mongoose.Schema.ObjectId, ref: "users" },
+});
+const users = mongoose.model("users", usersSchema);
+const articles = mongoose.model("articles", articlesSchema);
+const comments = mongoose.model("comment", commentsSchema);
+module.exports.users = users;
+module.exports.articles = articles;
+module.exports.comments = comments;
